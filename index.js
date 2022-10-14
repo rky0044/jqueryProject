336c7066dbfcfa248bce76e958e87bf9145a1852
rky0044 <rakesh@gohashinclude.com>
2022-10-14 14:36:06 +0530

    radio button complete in form3

--- index.js
+++ index.js
@@ -45,81 +45,105 @@ $(document).ready(function () {
 
       var hh = $(this).find(":selected").val();
       var kk = $("main section:nth-child(" + hh + ") div").each(function (index) {
-         index1=index+1;
+         index1 = index + 1;
          tt = $(this).text();
          $("#drpSubHeading").append(new Option(tt, index1));
 
       });
    });
-      
-      //formInput  --- form 3----
+
+   //formInput  --- form 3----
 
    $(".formInput").submit(function (event) {
-        
-         event.preventDefault();
-         //var fhh=$("#drpSubHeading :selected").text();
-         
-         //console.log(fhh,"seleellel")
-         var drpvalue = $("#inputtype").val();
-        
-         var inputName = $("#inputName").val();
 
-         var inputLabel=$("#inputLabel").val();
-         var inputClass=$("#inputClass").val();
-         var inputValue=$("#inputValue").val();
-         var inputPlaceholder=$("#inputPlaceholder").val();
-        
-         var inputReadonly=$("#inputReadonly").val();
-         var inputDisabled=$("#inputDisabled").val();
-         var required=$("#required").val();
-         var formheading=parseInt($("#formHeading :selected").val());
-         var formhSubeading=parseInt( $("#drpSubHeading :selected").val());  
-         var inputOption=$("#inputOption").val();
-                  
-         if(drpvalue == "radio"){
-            console.log(drpvalue)
-         }
-         else if(drpvalue == "checkbox"){
-            console.log(drpvalue)
-         }
+      event.preventDefault();
+      //var fhh=$("#drpSubHeading :selected").text();
 
-         else if(drpvalue == "select"){
-            var arroption= inputOption.split(',');
-            console.log(arroption);
-            var ptag=$('<p></p>');
-            var select =$("<select class="+inputClass+" name="+inputName+"></select>").appendTo(ptag);
+      //console.log(fhh,"seleellel")
+      var drpvalue = $("#inputtype").val();
 
-           
-           //$(select).prepend("<p></p>");
+      var inputName = $("#inputName").val();
+
+      var inputLabel = $("#inputLabel").val();
+      var inputClass = $("#inputClass").val();
+      var inputValue = $("#inputValue").val();
+      var inputPlaceholder = $("#inputPlaceholder").val();
+
+      var inputReadonly = $("#inputReadonly").val();
+      var inputDisabled = $("#inputDisabled").val();
+      var required = $("#required").val();
+      var formheading = parseInt($("#formHeading :selected").val());
+      var formhSubeading = parseInt($("#drpSubHeading :selected").val());
+      var inputOption = $("#inputOption").val();
+
+      if (drpvalue == "radio") {
+         var arroption = inputOption.split(',');
+         console.log(arroption);
+         var ptag = $('<p></p>');
+         var radio = $(ptag).appendTo($("main section:nth-child(" + formheading + ") div:nth-child(" + (formhSubeading + 1) + ")"))
 
 
-            $(arroption).each(function(key,value ){
-                  key=key+1;
-                  var option = $("<option></option>");
-                  option.val(key);
-                  option.html(value);
-                  $(option).appendTo(select);
-            })
-          
-          //  
-           // console.log(select)
-          //$('<p></p>').appendTo($("main section:nth-child("+formheading+") div:nth-child("+(formhSubeading+1)+")"));
-            
-          $(ptag).appendTo($("main section:nth-child("+formheading+") div:nth-child("+(formhSubeading+1)+")"));
-           
-         }
-         else if(drpvalue == "file"){
-            console.log(drpvalue)
-         }
-         else if(drpvalue == "textarea"){
-            $("main section:nth-child("+formheading+") div:nth-child("+(formhSubeading+1)+")").append("<p><label>"+inputLabel+"</label><textarea name="+inputName+" class="+inputClass+" placeholder="+inputPlaceholder+"></textarea></p>"); //add input box
-         }
-         else{
-         
-            $("main section:nth-child("+formheading+")  div:nth-child("+(formhSubeading+1)+")").append("<p><label>"+inputLabel+"</label><input type="+drpvalue+" name="+inputName+" class="+inputClass+" value="+inputValue+" placeholder="+inputPlaceholder+"></p>"); //add input box
-         }
+         $(arroption).each(function (index, value) {
+            indexxx = index + 1;
+            $(radio).append(
+               $('<input>').prop({
+                  type: drpvalue,
+                  id: indexxx,
+                  name: inputName,
+                  value: value
+               })
+            ).append(
+               $('<label>').prop({
+                  for: value
+               }).html(value)
+            )
 
-         
+         })
+
+
+      }
+      else if (drpvalue == "checkbox") {
+         console.log(drpvalue)
+      }
+
+      else if (drpvalue == "select") {
+         var arroption = inputOption.split(',');
+         console.log(arroption);
+         var ptag = $('<p></p>');
+         var select = $("<select class=" + inputClass + " name=" + inputName + "></select>").appendTo(ptag);
+         console.log(select)
+
+
+         //$(select).prepend("<p></p>");
+
+
+         $(arroption).each(function (key, value) {
+            key = key + 1;
+            var option = $("<option></option>");
+            option.val(key);
+            option.html(value);
+            $(option).appendTo(select);
+         })
+
+         //  
+         // console.log(select)
+         //$('<p></p>').appendTo($("main section:nth-child("+formheading+") div:nth-child("+(formhSubeading+1)+")"));
+
+         $(ptag).appendTo($("main section:nth-child(" + formheading + ") div:nth-child(" + (formhSubeading + 1) + ")"));
+
+      }
+      else if (drpvalue == "file") {
+         console.log(drpvalue)
+      }
+      else if (drpvalue == "textarea") {
+         $("main section:nth-child(" + formheading + ") div:nth-child(" + (formhSubeading + 1) + ")").append("<p><label>" + inputLabel + "</label><textarea name=" + inputName + " class=" + inputClass + " placeholder=" + inputPlaceholder + "></textarea></p>"); //add input box
+      }
+      else {
+
+         $("main section:nth-child(" + formheading + ")  div:nth-child(" + (formhSubeading + 1) + ")").append("<p><label>" + inputLabel + "</label><input type=" + drpvalue + " name=" + inputName + " class=" + inputClass + " value=" + inputValue + " placeholder=" + inputPlaceholder + "></p>"); //add input box
+      }
+
+
 
    })
 
